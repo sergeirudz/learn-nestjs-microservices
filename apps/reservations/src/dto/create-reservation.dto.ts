80b748938ae3ba15e249966091cfe0efaf0452a8
Sergei Rudz <31508949+sergeirudz@users.noreply.github.com>
2023-05-29 14:29:43 +0300

    4. Validation & Logging

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,20 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date) // when you pass "12/20/2022" in body, it will be converted to Date object
   startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
-  invoice: string;
+
+  @IsString()
+  @IsNotEmpty()
+  invoiceId: string;
 }
